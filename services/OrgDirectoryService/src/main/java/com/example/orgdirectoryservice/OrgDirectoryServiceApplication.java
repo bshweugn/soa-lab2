2dc3fc3a4b5fa8947591d4ad560a2a49f08ed608
Hokure <hokure.04@gmail.com>
2025-10-17 19:58:44 +0300

    upd: second service added and resources moved to gitignore

---
+++ services/OrgDirectoryService/src/main/java/com/example/orgdirectoryservice/OrgDirectoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.orgdirectoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgDirectoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgDirectoryServiceApplication.class, args);
+    }
+
+}
