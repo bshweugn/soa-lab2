505ad7a34e2b47006c58383905a82a18fe06d9a5
hokure <hokure.04@gmail.com>
2025-10-23 22:59:10 +0300

    fix: ok now that's monster work with our service

---
+++ services/OrgDirectoryService/src/main/java/com/example/orgdirectoryservice/config/DataSourceConfigurer.java
@@ -0,0 +1,20 @@
+package com.example.orgdirectoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfigurer {
+
+    private static final String JNDI = "jdbc/orgdirectory_service";
+
+    @Bean(destroyMethod = "")
+    public DataSource dataSource() throws DataSourceLookupFailureException {
+        JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+        return dataSourceLookup.getDataSource(JNDI);
+    }
+}
---
+++ services/OrgDirectoryService/src/main/java/com/example/orgdirectoryservice/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.example.orgdirectoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setExposedHeaders(List.of("*"));
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(source);
+    }
+
+}
